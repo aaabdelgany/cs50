2ed857efba7abf22f83147f46a06fd7b42e43c49
aaatest-1 <95378815+aaatest-1@users.noreply.github.com>
2021-12-11 16:29:14 -0500

    Update views.py

--- project5_abduHR/abduHR/views.py
+++ project5_abduHR/abduHR/views.py
@@ -84,6 +84,7 @@ def impemp(request):
             if not re.search(phone_regex,row[3].strip()):
                 return render(request,"abduHR/impemp.html",{
                     "message":"Your import file contains asdasdinvalid phone number(s). Phone numbers must be entered in 10 digit numeric format"
+                    #testing nah
                 })
             if not re.search(email_regex,row[2].strip()):
                 return render(request,"abduHR/impemp.html",{
